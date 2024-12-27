d9ff6ff57034fb2cdab9cd0e6caead776cf3111c
ngoquangtu <ngoquangtuhd@gmail.com>
2024-12-27 20:39:36 +0700

    init

---
+++ Assets/Scripts/Player/PlayerWin.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GD3D.Player
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class PlayerWin : PlayerScript
+    {
+
+
+        public override void Start()
+        {
+            base.Start();
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+
+        public override void FixedUpdate()
+        {
+            base.FixedUpdate();
+        }
+    }
+}
